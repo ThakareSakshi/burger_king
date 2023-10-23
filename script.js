b399776f578b63351b786f84c0940287adc96763
ThakareSakshi <sakshithakare121@gmail.com>
2023-10-23 09:59:07 +0530

    added order id

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let order_section=document.querySelector(".order");
+let orderid=432;
 
 const order_btn=document.querySelector(".order_btn");
 order_btn.addEventListener("click",()=>{
     let burger=document.getElementById("burger");
     let fries=document.getElementById("Fries");
     let coldDrink=document.getElementById("cold-Drink");
+
+    let orderID=document.querySelector(".order_id");
+    orderID.innerText=orderid++;
 
     if(burger.checked==true){
        console.log("burger");
